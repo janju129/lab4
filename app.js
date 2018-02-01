1775370113d07b8c6f5371e8834fea341b70c616
Janselle Justo <jcjusto@ucsd.edu>
2018-02-01 01:50:00 -0800

    node.js lab

--- app.js
+++ app.js
@@ -38,9 +38,18 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+
+var index = require("./routes/index");
+var project = require("./routes/project");
+
+app.get('/', index.view);
+app.get('/project', project.viewProject);
+
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
